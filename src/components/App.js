e15edd5d7fcaaf3d18e87599e8c8311c509526cc
Dallin Breen <dallinbreen624@gmail.com>
2022-05-02 11:43:05 -0600

    login screen updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="login">
+        <form className="login-form">
+          <h1>Breen Chat</h1>
+          <label>Enter Username:</label>
+          <input type="text" id="username" />
+          <label>Enter Password:</label>
+          <input type="password" id="password" />
+          <button>SUBMIT</button>
+        </form>
+      </div>
     </div>
   );
 }
