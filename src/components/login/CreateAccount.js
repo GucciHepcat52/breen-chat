036fa8bd2a62fdaf29594e25c90f93b620c70e3d
GucciHepcat52 <dallinbreen624@gmail.com>
2022-05-11 20:58:23 -0600

    added the create acccount page

--- src/components/login/CreateAccount.js
+++ src/components/login/CreateAccount.js
@@ -1,9 +1,24 @@
 import React from "react";
+import "../App.css";
+import { Link } from "react-router-dom";
 
 export default function CreateAccount() {
-    return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
-    )
+  return (
+    <div className="login">
+      <form className="login-form">
+        <label>Enter Username:</label>
+        <input type="text" id="username" />
+        <label>Enter First Name:</label>
+        <input type="text" id="fname" />
+        <label>Enter Last Name:</label>
+        <input type="text" id="lname" />
+        <label>Enter Email:</label>
+        <input type="email" id="email" />
+        <label>Enter Password:</label>
+        <input type="password" id="password" />
+        <button>SUBMIT</button>
+      </form>
+      <Link to="/">Back To Login</Link>
+    </div>
+  );
 }
