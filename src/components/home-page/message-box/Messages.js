379a98a2f2a72ed96828b4e00723e5556f1b7c54
Dallin Breen <dallinbreen624@gmail.com>
2022-05-19 21:01:06 -0600

    updated message box

--- src/components/home-page/message-box/Messages.js
+++ src/components/home-page/message-box/Messages.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "../../App.css";
-import Input from './Input';
+import Input from "./Input";
 
 export default function Messages() {
   return (
     <div className="message-box">
       <Input />
-      <h1>Hello World</h1>
+      <div className="message-fam"></div>
+      <div className="message-self"></div>
+      <div className="message-fam"></div>
+      <div className="message-self"></div>
     </div>
   );
 }
